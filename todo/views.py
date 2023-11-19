7407af8cfad66b66f482ad9e6629c7ad826764de
Daniel Potts. <djpotts2891@gmail.com>
2023-11-19 10:29:59 +0000

    Modify form to build from Django

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Item
+from .forms import ItemForm
 
 
 # Create your views here.
@@ -11,14 +12,19 @@ def get_todo_list(request):
     }
     return render(request, 'todo/todo_list.html', context)
 
+
 def add_item(request):
     '''Add a new item to the todo list'''
-    #POST request
+    # POST request
     if request.method == 'POST':
-        name = request.POST.get('item_name')
-        done = 'item_done' in request.POST
-        Item.objects.create(name=name, done=done)
-        return redirect('get_todo_list')
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('get_todo_list')
 
-    #GET request
-    return render(request, 'todo/add_item.html')
+    # GET request
+    form = ItemForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'todo/add_item.html', context)
