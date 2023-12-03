bc185a04f60e3ffe6b937f13c4f280ea3cde2b54
Daniel Potts. <djpotts2891@gmail.com>
2023-12-03 13:46:36 +0000

    Update to PostGresDB for Heroku

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -16,6 +16,8 @@ import os
 
 import psycopg2
 
+import dj_database_url
+
 if os.path.exists("env.py"):
     import env
     
@@ -82,10 +84,7 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
