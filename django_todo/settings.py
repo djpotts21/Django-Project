afd7a9963ecc1b90eb68242db18045632e31af12
Daniel Potts. <djpotts2891@gmail.com>
2023-12-04 21:31:47 +0000

    Update settings to create development variable

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -20,6 +20,8 @@ import dj_database_url
 
 if os.path.exists("env.py"):
     import env
+
+development = os.getenv("DEVELOPMENT", "FALSE")
     
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -32,7 +34,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG")
+DEBUG = development
 
 ALLOWED_HOSTS = [os.environ.get("HEROKU_HOSTNAME")]
 
@@ -83,9 +85,17 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
+if development:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR), 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
 
 
 # Password validation
