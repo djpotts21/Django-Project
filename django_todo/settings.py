a832a1bad549f0987ffe1a8487d848f09af1e09d
Daniel Potts. <djpotts2891@gmail.com>
2023-12-04 21:41:19 +0000

    Fix false in Development Settings

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -21,7 +21,7 @@ import dj_database_url
 if os.path.exists("env.py"):
     import env
 
-development = os.getenv("DEVELOPMENT", "FALSE")
+development = os.getenv("DEVELOPMENT", False)
     
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
